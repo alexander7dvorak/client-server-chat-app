0ef851190c4aa6428d24ff2e32bbd3b4f7250e3b
alexander7dvorak <olexander.dvorak@outlook.com>
2023-02-13 17:56:55 +0200

    Added lombok dependency. Added ChatMessage class to the new package 'model'.

---
+++ src/main/java/com/chatonwebsockets/model/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.chatonwebsockets.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ChatMessage {
+    @Getter @Setter
+    private MessageType type;
+    @Getter @Setter
+    private String content;
+    @Getter @Setter
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
